996df195cfec25812ca9de6c534aeeeef639b8fd
sebseb7 <github@seb.exse.net>
2012-09-07 05:46:18 +0200

    whitespace

--- main.c
+++ main.c
@@ -70,22 +70,22 @@ int main(int argc, char *argv[]) {
 
 	keyboard_init();
 
-        struct termios tio;
- 
- 
-        memset(&tio,0,sizeof(tio));
-        tio.c_iflag=0;
-        tio.c_oflag=0;
-        tio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
-        tio.c_lflag=0;
-        tio.c_cc[VMIN]=1;
-        tio.c_cc[VTIME]=5;
- 
-        tty_fd=open("/dev/ttyACM1", O_RDWR | O_NONBLOCK);      
-        cfsetospeed(&tio,B115200);            // 115200 baud
-        cfsetispeed(&tio,B115200);            // 115200 baud
- 
-        tcsetattr(tty_fd,TCSANOW,&tio);
+	struct termios tio;
+
+
+	memset(&tio,0,sizeof(tio));
+	tio.c_iflag=0;
+	tio.c_oflag=0;
+	tio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
+	tio.c_lflag=0;
+	tio.c_cc[VMIN]=1;
+	tio.c_cc[VTIME]=5;
+
+	tty_fd=open("/dev/ttyACM1", O_RDWR | O_NONBLOCK);      
+	cfsetospeed(&tio,B115200);            // 115200 baud
+	cfsetispeed(&tio,B115200);            // 115200 baud
+
+	tcsetattr(tty_fd,TCSANOW,&tio);
 
 	for(x = 0; x < LED_WIDTH; x++) {
 		for(y = 0; y < LED_HEIGHT; y++) {
